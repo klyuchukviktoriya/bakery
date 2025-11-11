a9ca1ea509b339167b6f20f56c3a76b1259ef687
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2025-11-11 05:36:41 +0200

    responsive modal, formats, swiper

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
-const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnClose = document.querySelector('.modal-btn-close');
+const modal = document.querySelector(".backdrop");
+const modalBtnsOpen = document.querySelectorAll(".modal-btn-open");
+const modalBtnClose = document.querySelector(".modal-btn-close");
+const toggleModal = () => modal.classList.toggle("is-hidden");
 
-const toggleModal = () => modal.classList.toggle('is-hidden');
+modalBtnsOpen.forEach(btn => btn.addEventListener("click", toggleModal));
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener("click", toggleModal);
