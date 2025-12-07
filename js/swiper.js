464141f3084beae96fe883c7b1a66abc7c8bac81
klyuchukviktoriya <klyuchuk.viktoriya@gmail.com>
2025-12-07 17:52:35 +0200

    swiper optimization

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,33 @@
-const swiper = new Swiper(".swiper", {
-  loop: true,
-  slidesPerView: 1,
-  breakpoints: {
-    768: {
-      slidesPerView: 2
+const swiperEl = document.querySelector(".swiper");
+
+const initSwiper = () => {
+  new Swiper(".swiper", {
+    speed: 400,
+    slidesPerView: 1,
+
+    breakpoints: {
+      768: { slidesPerView: 2 },
+      1280: { slidesPerView: 3 }
     },
-    1280: {
-      slidesPerView: 3
+
+    pagination: {
+      el: ".pagination",
+      bulletClass: "pagination__button",
+      bulletActiveClass: "pagination__button--active"
+    },
+
+    navigation: {
+      nextEl: ".formats__button--next",
+      prevEl: ".formats__button--prev"
     }
-  },
-  pagination: {
-    el: ".pagination",
-    bulletClass: "pagination__button",
-    bulletActiveClass: "pagination__button--active"
-  },
-  navigation: {
-    nextEl: ".formats__button--next",
-    prevEl: ".formats__button--prev"
+  });
+};
+
+const observer = new IntersectionObserver(entries => {
+  if (entries[0].isIntersecting) {
+    initSwiper();
+    observer.disconnect();
   }
 });
+
+observer.observe(swiperEl);
